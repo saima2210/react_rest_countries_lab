1b9cb236264d1d6d30ee820ba8cde09b9696bd36
Saima Miah <saima_miah@hotmail.co.uk>
2023-12-05 18:04:11 +0000

    all countries listed and can move to visited list :)

--- src/container/CountriesContainer.js
+++ src/container/CountriesContainer.js
@@ -9,66 +9,35 @@ const CountriesContainer = () => {
     const [visitedCountries, setVisitedCountries] = useState([]);
 
 
-const loadJapanData = async () => {
-    const japanResponse = await fetch ("https://restcountries.com/v3.1/name/japan");
-    const japanData = await japanResponse.json();
+const loadData = async () => {
+    const response = await fetch ("https://restcountries.com/v3.1/all");
+    const data = await response.json();
     
 
-    setCountries(japanData);
+    setCountries(data);
 
 };
 
-// const loadTanazaniaData = async () => {
-//     const tanzaniaResponse = await fetch ("https://restcountries.com/v3.1/name/tanzania");
-//     const tanzaniaData = await tanzaniaResponse.json();
 
-//     setCountries(tanzaniaData);
-
-// }; - tried to get these two to list out but then it would overwrite japan....
-
-
-
-const loadVisitedCountriesData = async () => { // tried to add more countries but it would overwrite 
-      const visitedResponse = await fetch("https://restcountries.com/v3.1/name/thailand");
-      const visitedData = await visitedResponse.json();
-      setVisitedCountries(visitedData);
-  };
-
+const handleCountryVisited = (country) => {
+    setCountries((visitedCountries) => visitedCountries.filter((currentCountry) => currentCountry !== country));
+    setVisitedCountries((hasVisitedCountry) => [...hasVisitedCountry, country]);
+};
 
 
     useEffect( () => {
-        loadJapanData();
-        // loadTanazaniaData
-        loadVisitedCountriesData();
+        loadData();
+
     }, []);
 
     return (  
         <>
         <h1>My country bucket list 🌋</h1>
-        <CountriesList countries={countries} />
+        <CountriesList countries={countries} onCountryVisited={handleCountryVisited}/>
         <VisitedCountriesList visitedCountries={visitedCountries} />
     </>
   );
 };
 
-
-
-
-
-
-
-
-
-//         <h1>My country bucket list 🌋</h1>
-//         <ul>
-//             <CountriesList countries={countries}/>
-
-
-//             <CountriesList countries={visitedCountries}/>
-//             </ul>
-//         </>
-//     );
-
-// };
  
 export default CountriesContainer;
